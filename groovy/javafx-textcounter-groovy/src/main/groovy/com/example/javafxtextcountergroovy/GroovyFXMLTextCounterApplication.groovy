00e1c52dcb59b459cac39ad6c5e02894bb64ff9e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2023-11-30 20:50:50 -0600

    Fix errors

--- groovy/javafx-textcounter-groovy/src/main/groovy/com/example/javafxtextcountergroovy/GroovyFXMLTextCounterApplication.groovy
+++ groovy/javafx-textcounter-groovy/src/main/groovy/com/example/javafxtextcountergroovy/GroovyFXMLTextCounterApplication.groovy
@@ -13,7 +13,7 @@ import java.io.IOException
         FXMLLoader fxmlLoader = new FXMLLoader(
                 com.example.javafxtextcountergroovy.GroovyFXMLTextCounterApplication.class
                         .getResource("fxml-text-counter-groovy.fxml"))// load FXML file resource
-        Scene scene = new Scene(fxmlLoader.load(), 480, 320)// set application size to 480x320
+        Scene scene = new Scene(fxmlLoader.load() as Parent, 480, 320)// set application size to 480x320
         stage.setTitle("Text Counter")// set title of stage
         stage.setScene(scene)// set stage to scene
         stage.show()// show the stage
